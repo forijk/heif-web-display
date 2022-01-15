2811c89a05c7ebf0e1f1c37ea08b3415ec759fce
SwimmingTiger <hu60.cn@gmail.com>
2022-01-15 18:14:25 +0800

    修复透明通道导致的图片扭曲变形问题

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-const workerUrl = '/tpl/jhin/js/heif-web-display/dist/worker.js?r=5';
-const worker = new Worker(workerUrl);
-const promisePool = {};
+(function() {
+  const workerUrl = '/tpl/jhin/js/heif-web-display/dist/worker.js?r=5';
+  const worker = new Worker(workerUrl);
+  const promisePool = {};
 
-worker.onmessage = e => {
-  promisePool[e.data.url](e.data.urlPng);
-  console.log('ConvertHeicToPng done:', e.data.url);
-};
+  worker.onmessage = e => {
+    promisePool[e.data.url](e.data.urlPng);
+    console.log('ConvertHeicToPng done:', e.data.url);
+  };
 
-async function ConvertHeicToPng(url) {
-  console.log('ConvertHeicToPng:', url);
-  const promise = new Promise(function(resolve) {
-    promisePool[url] = resolve;
-    worker.postMessage({'url': url});
-  });
-  return promise;
-}
-document.ConvertHeicToPng = ConvertHeicToPng;
+  async function ConvertHeicToPng(url) {
+    console.log('ConvertHeicToPng:', url);
+    const promise = new Promise(function(resolve) {
+      promisePool[url] = resolve;
+      worker.postMessage({'url': url});
+    });
+    return promise;
+  }
+  document.ConvertHeicToPng = ConvertHeicToPng;
+})()
--- src/worker.js
+++ src/worker.js
@@ -1,80 +1,87 @@
-importScripts('/tpl/jhin/js/heif-web-display/dist/wasm_heif.js');
-importScripts('/tpl/jhin/js/heif-web-display/dist/wasm_mozjpeg.js');
+(function() {
+  importScripts('/tpl/jhin/js/heif-web-display/dist/wasm_heif.js');
+  importScripts('/tpl/jhin/js/heif-web-display/dist/wasm_mozjpeg.js');
 
-const cacheName = 'ConvertHeicToPng';
-const defaultJpegOption = {
-  quality: 75,
-  baseline: false,
-  arithmetic: false,
-  progressive: true,
-  optimize_coding: true,
-  smoothing: 0,
-  in_color_space: 2, // J_COLOR_SPACE.JCS_RGB
-  out_color_space: 3, // J_COLOR_SPACE.JCS_YCbCr
-  quant_table: 3,
-  trellis_multipass: false,
-  trellis_opt_zero: false,
-  trellis_opt_table: false,
-  trellis_loops: 1,
-  auto_subsample: true,
-  chroma_subsample: 2,
-  separate_chroma_quality: false,
-  chroma_quality: 75,
-};
+  const cacheName = 'ConvertHeicToPng';
+  // 3 for RGB, 4 for RGBA (must adapt options.in_color_space!)
+  const defaultJpegChannels = 4;
+  const defaultJpegOption = {
+    quality: 75,
+    baseline: false,
+    arithmetic: false,
+    progressive: true,
+    optimize_coding: true,
+    smoothing: 0,
+    // 2 for JCS_RGB, 6 for RGB, 12 for RGBA
+    // see J_COLOR_SPACE in wasm_mozjpeg.d.ts
+    in_color_space: 12,
+    out_color_space: 3, // J_COLOR_SPACE.JCS_YCbCr
+    quant_table: 3,
+    trellis_multipass: false,
+    trellis_opt_zero: false,
+    trellis_opt_table: false,
+    trellis_loops: 1,
+    auto_subsample: true,
+    chroma_subsample: 2,
+    separate_chroma_quality: false,
+    chroma_quality: 75,
+  };
 
-async function convertHeicToPng(url) {
-  // find blob from cache
-  /*try {
-    const cache = await caches.open(cacheName);
-    const response = await cache.match(new Request(url));
-    if (response) {
-      const blob = await response.blob();
-      if (blob.type == 'image/jpeg') {
-        console.log('Found from Cache:', url);
-        return URL.createObjectURL(blob);
+  async function convertHeicToPng(url) {
+    // find blob from cache
+    /*try {
+      const cache = await caches.open(cacheName);
+      const response = await cache.match(new Request(url));
+      if (response) {
+        const blob = await response.blob();
+        if (blob.type == 'image/jpeg') {
+          console.log('Found from Cache:', url);
+          return URL.createObjectURL(blob);
+        }
       }
-    }
-  } catch(e) {
-    // ignore
-    console.log(e)
-  }*/
+    } catch(e) {
+      // ignore
+      console.log(e)
+    }*/
 
-  const blob = await fetch(url)
-    .then(async (data) => {
-      const array = new Uint8Array(await data.arrayBuffer());
-      const heif = await wasm_heif({
-        onRuntimeInitialized() {
-          console.log('wasm_heif loaded');
-        },
+    const blob = await fetch(url)
+      .then(async (data) => {
+        const array = new Uint8Array(await data.arrayBuffer());
+        const heif = await wasm_heif({
+          onRuntimeInitialized() {
+            console.log('wasm_heif loaded');
+          },
+        });
+        const rgba = heif.decode(array, array.length, true);
+        const dim = heif.dimensions();
+        heif.free();
+
+        const jpeg = await wasm_mozjpeg({
+          onRuntimeInitialized() {
+            console.log('wasm_mozjpeg loaded');
+          },
+        });
+        let output = jpeg.encode(rgba, dim.width, dim.height,
+          defaultJpegChannels, defaultJpegOption);
+        jpeg.free();
+
+        return new Blob([output], { type: 'image/jpeg' });
       });
-      const rgb = heif.decode(array, array.length, false);
-      const dim = heif.dimensions();
-      heif.free();
 
-      const jpeg = await wasm_mozjpeg({
-        onRuntimeInitialized() {
-          console.log('wasm_mozjpeg loaded');
-        },
-      });
-      let output = jpeg.encode(rgb, dim.width, dim.height, dim.channels, defaultJpegOption);
-      jpeg.free();
+    // cache blob
+    /*try {
+      const cache = await caches.open(cacheName);
+      cache.put(new Request(url), new Response(blob));
+    } catch(e) {
+      // ignore
+      console.log(e)
+    }*/
+    return URL.createObjectURL(blob);
+  }
 
-      return new Blob([output], { type: 'image/jpeg' });
-    });
-
-  // cache blob
-  /*try {
-    const cache = await caches.open(cacheName);
-    cache.put(new Request(url), new Response(blob));
-  } catch(e) {
-    // ignore
-    console.log(e)
-  }*/
-  return URL.createObjectURL(blob);
-}
-
-// web worker
-onmessage = async function (e) {
-  e.data.urlPng = await convertHeicToPng(e.data.url);
-  postMessage(e.data);
-}
+  // web worker
+  onmessage = async function (e) {
+    e.data.urlPng = await convertHeicToPng(e.data.url);
+    postMessage(e.data);
+  }
+})()
